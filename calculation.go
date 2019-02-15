1905939332da9b4bd9c91bad7c0e638ffa4c0d06
ClaireBvs <clairembeauvais@gmail.com>
2019-02-14 21:21:23 -0700

    Refactor #main to use colon equal operator to create variable

--- calculation.go
+++ calculation.go
@@ -5,16 +5,11 @@ import (
 )
 
 func main() {
-  var x float64
-  var y float64
-
-  x = 4
-  y = 2
+  x, y := 1.0, 2.0
 
   fmt.Printf("x=%v, type of %T\n", x, x)
   fmt.Printf("y=%v, type of %T\n", y, y)
 
-  var mean float64
-  mean = (x + y)/2
+  mean := (x + y) / 2.0
   fmt.Printf("The result is %v, type of %T\n", mean, mean)
 }
