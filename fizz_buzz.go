2262ada9f36ccb89113b62849447809f16381fd6
ClaireBvs <clairembeauvais@gmail.com>
2019-03-19 22:05:42 -0600

    Solving fizz buzz using golang

---
+++ fizz_buzz.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  for i := 0; i <= 50; i++ {
+    if (i%3 == 0) && (i%5 == 0) {
+      fmt.Println("fizz buzz")
+    } else if i%3 == 0 {
+      fmt.Println("fizz")
+    } else if i%5 == 0 {
+      fmt.Println("buzz")
+    } else {
+      fmt.Println(i)
+    }
+  }
+}
